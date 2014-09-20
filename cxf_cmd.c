bdc981f54dc5d313f441c9f359364cde999c2b25
Jason Cai <acecxf@126.com>
2014-09-20 17:54:04 +0800

    first commit baseline version

---
+++ cxf_cmd.c
@@ -0,0 +1,60 @@
+/**
+ * File 			: cxf_cmd.c
+ * This file is my test command.
+ * file list.
+ * Author			: Cai XiangFeng
+ * 
+ * Change Logs:
+ * Date 		Author 			Notes
+ * 2014-09-20 	Jason Cai 		first version
+ */
+/*#if defined(RTGUI_USING_DFS_FILERW) || defined(RTGUI_USING_STDIO_FILERW)*/
+#include <dfs_posix.h>
+#define PATH_SEPARATOR		'/'
+#include <string.h>
+#include <finsh.h>
+
+/**
+ * List files in given directory
+ */
+void cxf_list_dirs(const char *directory) 
+{
+	DIR *dir;
+	struct stat s;
+	rt_uint32_t index;
+	rt_uint32_t count;
+	struct dirent *dirent;
+
+	if (directory == RT_NULL) {
+		directory = "/";
+	}
+
+	/*
+	fullpath = rtgui_malloc(256);
+	if (fullpath == RT_NULL) {
+		rt_kprintf("[CXF] error: OOM\r\n");
+		return;
+	}
+	*/
+	
+	dir = opendir(directory);
+	if (dir == RT_NULL) {
+		rt_kprintf("[CXF] error: opendir\r\n");
+		return;
+	}
+	
+	do {
+		dirent = readdir(dir);
+		if (dirent == RT_NULL) break;
+		rt_kprintf("[CXF] d_name: %s\r\n", dirent->d_name);
+		if (strcmp(dirent->d_name, ".") == 0) continue;
+		if (strcmp(dirent->d_name, "..") == 0) continue;
+		++count;
+	} while (dirent != RT_NULL);
+	
+	rt_kprintf("[CXF] %d files in %s\r\n", count, directory);
+	
+	closedir(dir);
+}
+FINSH_FUNCTION_EXPORT(cxf_list_dirs, list files in given directory);
+/*#endif*/
